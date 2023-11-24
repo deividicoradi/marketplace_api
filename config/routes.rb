5279a05b4fdedfbb2a8d262d4fd92c7151c24042
deividicoradi <deividi.coradii@gmail.com>
2023-11-24 14:03:51 -0300

    Remove comments and add namespace to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@
 Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount Rswag::Ui::Engine => '/api-docs'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json },
+                              constant: { subdomanin: 'api'}, path: '/' do
+  end
 end
